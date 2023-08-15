fa96b409c52c208773c84b150f5e8f8af5985337
dsanup <anup12.m@gmail.com>
2023-08-15 11:19:29 +0600

    exception handling

--- exception/exception.py
+++ exception/exception.py
@@ -1,2 +1,12 @@
-numbers = [1, 3]
-print(numbers[3])
+try:
+    age = int(input("Age :"))
+    xfactor = 10 / age
+except ValueError as ex:
+    print("Please enter numeric value")
+    print(ex)
+    print(type(ex))
+except ZeroDivisionError:
+    print("Age cannot be divided by 0")
+else:
+    print("No exceptions were thrown.")
+print("Excution continues")
